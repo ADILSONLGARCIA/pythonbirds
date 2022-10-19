c361bd64b596cf499b52e363ac0979d1394a6882
Adilson Garcia <108878646+ADILSONLGARCIA@users.noreply.github.com>
2022-10-19 11:41:57 -0300

    Criado atributo de instância nome e idade na classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,20 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=48):
+        self.nome = nome
+        self.idade = idade
+
+
     def cumprimentar(self):
         return print('Olá!')
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Bolsonaro')
     print(Pessoa.cumprimentar(p))
     print()
+    print(p.cumprimentar())
     print(id(p))
     print()
-    print(p.cumprimentar())
+    p.nome = 'Adilson'
+    print(p.nome)
+    print(p.idade)
