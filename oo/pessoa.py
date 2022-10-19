8080599d48b959270d6410dc237ac839b0e2a9a3
Adilson Garcia <108878646+ADILSONLGARCIA@users.noreply.github.com>
2022-10-19 11:41:57 -0300

    Criado atributo de instância nome e idade e filho na classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,20 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=77):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
-        return print('Olá!')
+        return f'Olá!{id(self)}'
+
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print()
-    print(id(p))
-    print()
-    print(p.cumprimentar())
+    adilsonjr = Pessoa(nome='Adilson Jr')
+    adilson_pai = Pessoa(adilsonjr, nome='Adilson Pai')
+    Pessoa.cumprimentar(adilson_pai)
+    print(id(adilson_pai))
+    print(adilson_pai.cumprimentar())
+    print(adilson_pai.nome)
+    print(adilson_pai.idade)
+    for filho in adilson_pai.filhos:
+        print(filho.nome)
